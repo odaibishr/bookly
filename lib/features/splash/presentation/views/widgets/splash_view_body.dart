bfc59003c4e039fe326477142de54175333e5e8e
Odai Bishr <odaibishr@gmail.com>
2025-08-27 22:55:16 +0300

    refactor init state & navigate and create home

--- lib/features/splash/presentation/views/widgets/splash_view_body.dart
+++ lib/features/splash/presentation/views/widgets/splash_view_body.dart
@@ -1,5 +1,9 @@
+import 'package:bookly/constants.dart';
 import 'package:bookly/core/utils/assets.dart';
+import 'package:bookly/features/home/presentation/views/home_view.dart';
+import 'package:bookly/features/splash/presentation/views/widgets/sliding_text.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 class SplashViewBody extends StatefulWidget {
   const SplashViewBody({super.key});
@@ -12,16 +16,19 @@ class _SplashViewBodyState extends State<SplashViewBody>
     with SingleTickerProviderStateMixin {
   late AnimationController _controller;
   late Animation<Offset> animation;
+
   @override
   void initState() {
     super.initState();
-    _controller = AnimationController(
-      vsync: this,
-      duration: Duration(seconds: 1),
-    );
-    animation = Tween<Offset>(begin: const Offset(0, 20), end: Offset.zero).animate(_controller);
-    _controller.forward();
-    animation.addListener(() => setState(() {}));
+    initSlidingAnimation();
+
+    navigateToHome();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _controller.dispose();
   }
 
   @override
@@ -32,16 +39,31 @@ class _SplashViewBodyState extends State<SplashViewBody>
       children: [
         Image.asset(AssetsData.logo),
         const SizedBox(height: 4),
-        AnimatedBuilder(
-          animation: animation,
-          builder: (context, child) {
-            return Transform.translate(
-              offset: animation.value,
-              child: const Text("Read Free Books", textAlign: TextAlign.center),
-            );
-          },
-        ),
+        SlidingText(animation: animation),
       ],
     );
+  }
+
+  void initSlidingAnimation() {
+    _controller = AnimationController(
+      vsync: this,
+      duration: Duration(seconds: 1),
+    );
+    animation = Tween<Offset>(
+      begin: const Offset(0, 20),
+      end: Offset.zero,
+    ).animate(_controller);
+    _controller.forward();
+    animation.addListener(() => setState(() {}));
+  }
+
+  void navigateToHome() {
+    Future.delayed(Duration(seconds: 3), () {
+      Get.to(
+        () => const HomeView(),
+        transition: Transition.fadeIn,
+        duration: kTransitionDuration,
+      );
+    });
   }
 }
