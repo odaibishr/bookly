d8c8a7365ec4993355a522e5d13d8d7fa11fdeb5
Odai Bishr <odaibishr@gmail.com>
2025-09-01 23:38:23 +0300

    add rating to book details

--- lib/features/home/presentation/views/widgets/book_details_view_body.dart
+++ lib/features/home/presentation/views/widgets/book_details_view_body.dart
@@ -1,4 +1,5 @@
 import 'package:bookly/core/utils/styles.dart';
+import 'package:bookly/features/home/presentation/views/widgets/book_rating.dart';
 import 'package:bookly/features/home/presentation/views/widgets/custom_book_details_app_bar.dart';
 import 'package:bookly/features/home/presentation/views/widgets/custom_book_item.dart';
 import 'package:flutter/material.dart';
@@ -32,6 +33,8 @@ class BookDetailsViewBody extends StatelessWidget {
               style: Styles.textStyle18.copyWith(fontStyle: FontStyle.italic),
             ),
           ),
+          const SizedBox(height: 18),
+          const BookRating(mainAxisAlignment: MainAxisAlignment.center),
         ],
       ),
     );
--- lib/features/home/presentation/views/widgets/book_rating.dart
+++ lib/features/home/presentation/views/widgets/book_rating.dart
@@ -3,19 +3,27 @@ import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class BookRating extends StatelessWidget {
-  const BookRating({super.key});
+  const BookRating({
+    super.key,
+    this.mainAxisAlignment = MainAxisAlignment.start,
+  });
+  final MainAxisAlignment mainAxisAlignment;
 
   @override
   Widget build(BuildContext context) {
     return Row(
+      mainAxisAlignment: mainAxisAlignment,
       children: [
         const Icon(FontAwesomeIcons.solidStar, color: Colors.amber, size: 15),
         const SizedBox(width: 6.3),
         Text('4.8', style: Styles.textStyle16),
         const SizedBox(width: 5),
-        Text(
-          '(198)',
-          style: Styles.textStyle14.copyWith(color: const Color(0xff707070)),
+        Opacity(
+          opacity: 0.5,
+          child: Text(
+            '(198)',
+            style: Styles.textStyle14.copyWith(fontWeight: FontWeight.w600),
+          ),
         ),
       ],
     );
