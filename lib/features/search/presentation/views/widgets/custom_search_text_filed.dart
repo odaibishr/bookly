3f7853fd89c114283561d64c367e4d8db1b89586
Odai Bishr <odaibishr@gmail.com>
2025-09-03 20:32:52 +0300

    create search view

---
+++ lib/features/search/presentation/views/widgets/custom_search_text_filed.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class CustomSearchTextFiled extends StatelessWidget {
+  const CustomSearchTextFiled({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
